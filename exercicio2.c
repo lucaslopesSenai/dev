08973e2f3ca3e7b7a371d6d5ea6e998fd021e14e
lucaslopesSenai <lu.silva@fiemg.com.br>
2025-04-17 17:18:42 -0300

    enviando arquivos

---
+++ exercicio2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int senha = 0;
+
+    while (senha != 1234) {
+        printf("Digite a senha: ");
+        scanf("%d", &senha);
+
+        if (senha != 1234) {
+            printf("Senha incorreta. Tente novamente.\n");
+        }
+    }
+
+    printf("Acesso Liberado.\n");
+}
